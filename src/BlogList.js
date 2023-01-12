ff85cabe2e293b684bee968bab0b4407c64f1e39
Nour Kanout <mnkanout@gmail.com>
2023-01-12 15:16:16 +0100

    Tutorial-12 reusing components

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,7 +1,7 @@
 const BlogList = ({blogs, title}) => {
     return ( 
     <div className="blog-list">
-        <h2>Blogs list</h2>
+        <h2>{title}</h2>
         {blogs.map(blog => (
             <div className="blog-preview" key={blog.id}>
                 <h2>{blog.title}</h2>
--- src/Home.js
+++ src/Home.js
@@ -14,7 +14,8 @@ const Home = () => {
      
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} />
+            <BlogList blogs={blogs} title='All Blogs!' />
+            <BlogList blogs={blogs.filter((blog)=> blog.author === 'mario')} title="Mario's Blogs!"/>
         </div>
      );
 }
